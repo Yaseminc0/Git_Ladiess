8b7a282324d539eab7d7f95c19191adbfc7a9854
Yaseminc0 <yasemincalik77@gmail.com>
2023-11-01 19:30:20 +0000

    added changes to main file

--- src/MainPackage_week1/main_java.java
+++ src/MainPackage_week1/main_java.java
@@ -1,4 +1,22 @@
 package MainPackage_week1;
 
+import Yasemin.Week1_task1;
+
 public class main_java {
+    public static void main(String[] args) {
+
+        //space for rest of tasks
+        // test module issue
+
+
+        // String-2 > xyzThere (Yasemin)
+        System.out.println("String-2 > xyzThere");
+        System.out.println("Test case 1: ");
+        System.out.println(Week1_task1.xyzThere("abcxyz"));
+        System.out.println("Test case 2:");
+        System.out.println(Week1_task1.xyzThere("abc.xyz"));
+        System.out.println("Test case 3:");
+        System.out.println(Week1_task1.xyzThere("xyz.abc"));
+
+    }
 }
